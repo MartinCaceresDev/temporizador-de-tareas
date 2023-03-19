93d5858b1fb7e206604e74b6bbfeeb897d595bd8
Martin Caceres <martincaceres@outlook.com>
2023-03-18 22:21:30 -0300

    green when finished

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -136,7 +136,7 @@ export const Task = ({
   return (
     <>
       {alertActive && <Alert setAlertActive={setAlertActive} title={title} />}
-      <div className={`flex flex-col border rounded p-2 items-center relative ${counterFinished && 'bg-green-900/90'}`}>
+      <div className={`flex flex-col border rounded p-2 items-center relative ${(!hours && !minutes && !seconds) && 'bg-green-900/90'}`}>
 
         <div className='flex justify-between w-full'>
           {/* EDIT */}
