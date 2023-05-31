d252629e47644d41bcbbcabbede55111419c0e37
Martin Caceres <martincaceres@outlook.com>
2023-05-31 00:11:44 -0300

    pwa test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ export const Main = () => {
   const { tasks } = useTasksContext();
 
   return (
-    <main className={`bg-stone-300 rounded p-3 sm:p-5 flex flex-col w-full md:w-4/5 ${tasks?.length > 12 && 'lg:w-full'}`}>
+    <main className={`bg-stone-300 rounded p-3 pt-4 sm:p-5 flex flex-col w-full md:w-4/5 ${tasks?.length > 12 && 'lg:w-full'}`}>
       <Menu />
       <TasksContainer />
     </main>
