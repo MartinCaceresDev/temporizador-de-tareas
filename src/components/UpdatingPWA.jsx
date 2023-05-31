babc839a1491828a430cbbbe75aedd41a1461395
Martin Caceres <martincaceres@outlook.com>
2023-05-31 00:22:06 -0300

    pwa fix

--- src/components/UpdatingPWA.jsx
+++ src/components/UpdatingPWA.jsx
@@ -16,23 +16,23 @@ export const UpdatingPWA = () => {
 
   const close = () => setNeedRefresh(false);
 
-  return (
-    <div className='absolute flex flex-col items-center justify-evenly bg-gray-300 top-0 left-0 max-w-full sm:w-max h-min p-8 border-black border rounded-lg'>
-      <span className='font-semibold gap-4'>There is a new version available.</span>
-      <div className='flex justify-evenly'>
-        <button
-          onClick={() => updateServiceWorker(true)}
-          className='select-none cursor-pointer border bg-blue-700 hover:bg-blue-900 text-white font-semibold rounded-full py-1.5 px-3.5'
-        >
-          Update
-        </button>
-        <button
-          onClick={() => close()}
-          className='select-none border text-white rounded-full cursor-pointer bg-gray-500 hover:bg-gray-700 font-semibold py-1.5 px-3.5 transition-all'
-        >
-          Close
-        </button>
+  if (needRefresh) {
+    return (
+      <div className='absolute flex flex-col items-center justify-evenly bg-gray-300 top-0 left-0 max-w-full sm:w-max h-min p-8 border-black border rounded-lg'>
+        <span className='font-semibold gap-4'>There is a new version available.</span>
+        <div className='flex justify-evenly'>
+          <button
+            onClick={() => updateServiceWorker(true)}
+            className='select-none cursor-pointer border bg-blue-700 hover:bg-blue-900 text-white font-semibold rounded-full py-1.5 px-3.5'
+          >
+            Update
+          </button>
+          <button onClick={close} className='select-none border text-white rounded-full cursor-pointer bg-gray-500 hover:bg-gray-700 font-semibold py-1.5 px-3.5 transition-all'>
+            Close
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <></>;
 };
